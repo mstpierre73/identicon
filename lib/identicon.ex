3c1498ed7e8ac31ac18f798b35fc60d1fe38a685
Marise St-Pierre <marise.stpierre@nventive.com>
2022-12-23 15:24:48 -0500

    add functions to pick color and identicon grid

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,6 +3,8 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
+    |> build_grid
   end
 
   def hash_input(input) do
@@ -11,4 +13,23 @@ defmodule Identicon do
 
     %Identicon.Image{hex: hex}
   end
+
+  def pick_color(image) do
+    %Identicon.Image{hex: [r, g, b | _tail]} = image
+
+    %Identicon.Image{ image | color: {r, g, b}}
+  end
+
+  def build_grid(image) do
+    %Identicon.Image{hex: hex} = image
+    hex
+    |> Enum.chunk(3)
+    |> Enum.map(&mirror_row/1)
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+
+    row ++ [second, first]
+   end
 end
